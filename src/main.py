b9cfbd0597f77a5706ea9a08ece14065f283a9df
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-03-06 21:10:05 +0200

    Fastapi Quer pramter and Path prameter For Blog api

--- src/main.py
+++ src/main.py
@@ -7,10 +7,12 @@ app = FastAPI()
 
 
 @app.get('/')
-def index():
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
     # only get 10 published blogs
-        return {'data': f' published blogs from the db'}
-    
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
 @app.get('/blog/unpublished')
@@ -30,4 +32,12 @@ def comments(id, limit=10):
     return {'data': {'1', '2'}}
 
 
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
 
+
+@app.post('/blog')
+def create_blog(blog: Blog) :
+    return {'data': f"Blog is created with title as {blog.title}"}
