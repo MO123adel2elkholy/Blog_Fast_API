2d7fe7a8eeee446e4f0e365618cec779e357c8c1
mahmoud adel elkholy <adel333mahmoud@gmail.com>
2026-03-13 04:21:28 +0200

    back to sync  mood and solve bug

--- src/blog/database.py
+++ src/blog/database.py
@@ -3,36 +3,37 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-# sqlalchemy_uri_sync = 'sqlite:///.blog.db'
-sqlalchemy_uri_Async_driver = 'sqlite+aiosqlite:///.blog.db'
+sqlalchemy_uri_sync = 'sqlite:///.blog.db'
 
 
-# engine = create_engine(sqlalchemy_uri_sync, connect_args={
-#     'check_same_thread': False})
-
-engine = create_async_engine(sqlalchemy_uri_Async_driver, echo=True)
+engine = create_engine(sqlalchemy_uri_sync, connect_args={
+    'check_same_thread': False})
 
 
-# sessionlocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-Async_sessionlocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+sessionlocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
 
 Base = declarative_base()
 
 
-# def get_db():
-#     db = sessionlocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
-
-
-async def get_db():
-    async with Async_sessionlocal() as db:
+def get_db():
+    db = sessionlocal()
+    try:
         yield db
+    finally:
+        db.close()
 
 
-async def init_db():
-    """ initializes the database """
-    async with engine.begin() as connection:
-        await connection.run_sync(Base.metadata.create_all)
+# sqlalchemy_uri_Async_driver = 'sqlite+aiosqlite:///.blog.db'
+
+# engine = create_async_engine(sqlalchemy_uri_Async_driver, echo=True)
+# Async_sessionlocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+# async def get_db():
+#     async with Async_sessionlocal() as db:
+#         yield db
+
+
+# async def init_db():
+#     """ initializes the database """
+#     async with engine.begin() as connection:
+#         await connection.run_sync(Base.metadata.create_all)
